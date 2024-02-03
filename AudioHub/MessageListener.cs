37087a037fe376ae46cea56cab77459bfcdec5a8
Lucas Jorgenson <LJorgen12@outlook.com>
2024-02-03 17:28:27 -0500

    Greatly improved message reading

--- AudioHub/MessageListener.cs
+++ AudioHub/MessageListener.cs
@@ -2,12 +2,14 @@
 using Android.App;
 using Android.Content;
 using Android.Content.PM;
+using Android.Media;
 using Android.OS;
 using Android.Runtime;
 using Android.Service.Notification;
 using Android.Speech.Tts;
 using Android.Views;
 using Android.Widget;
+using Java.Util;
 using Javax.Security.Auth;
 using System;
 using System.Collections.Generic;
@@ -27,29 +29,47 @@ namespace AudioHub
 
         private string prevTitle;
         private string prevText;
-        private string prevSubText;
+
+        private Handler delayHandler;
+        private const long ttsDelayMillis = 500;
+        private VolumeShaper.Configuration volumeConfig;
+        private VolumeShaper volumeShaper;
 
         public override void OnNotificationPosted(StatusBarNotification sbn)
         {
-            if (!readMessages || sbn.Id == 2) return;
+            if (!readMessages || SongPlayer.mediaPlayer == null || !SongPlayer.mediaPlayer.IsPlaying || sbn.Id == 2) return;
             Bundle extras = sbn.Notification.Extras;
 
             string title = ConvertToASCIIIfNotAllEmojis(extras.GetString(Notification.ExtraTitle));
             string text = ConvertToASCIIIfNotAllEmojis(extras.GetString(Notification.ExtraText));
-            string subText = ConvertToASCIIIfNotAllEmojis(extras.GetString(Notification.ExtraSubText));
 
             if (title.Contains("Cable charging")) return;
-            if (title == prevTitle && text == prevText && subText == prevSubText) return;
+            if (title == prevTitle && text == prevText) return;
 
             prevTitle = title;
             prevText = text;
-            prevSubText = subText;
 
-            SongPlayer.mediaPlayer.SetVolume(.1f, .1f);
+            if (volumeShaper == null) volumeShaper = SongPlayer.mediaPlayer.CreateVolumeShaper(volumeConfig);
+            volumeShaper.Apply(VolumeShaper.Operation.Play);
 
-            if (title != null) tts.Speak(title, QueueMode.Add, paramsBundle, title);
-            if (text != null) tts.Speak(text, QueueMode.Add, paramsBundle, text);
-            if (subText != null) tts.Speak(subText, QueueMode.Add, paramsBundle, subText);
+            if (title != null) delayHandler.PostDelayed(() => tts.Speak(title, QueueMode.Add, paramsBundle, title), ttsDelayMillis);
+            if (text != null)
+            {
+                string[] lines = text.Split(Java.Lang.JavaSystem.GetProperty("line.separator"), StringSplitOptions.RemoveEmptyEntries);
+                
+                if (lines.Length < 2)
+                {
+                    delayHandler.PostDelayed(() => tts.Speak(text, QueueMode.Add, paramsBundle, text), ttsDelayMillis * 2);
+                }
+                else
+                {
+                    for (int i = 0; i < lines.Length; i++)
+                    {
+                        delayHandler.PostDelayed(() => tts.Speak(lines[i],
+                            QueueMode.Add, paramsBundle, lines[i]), ttsDelayMillis * (2 + i));
+                    }
+                }
+            }
         }
         private string ConvertToASCII(string str)
         {
@@ -62,17 +82,28 @@ namespace AudioHub
         }
         public override void OnListenerConnected()
         {
+            volumeConfig = new VolumeShaper.Configuration.Builder()
+                .SetCurve(new[] { 0f, 1f }, new[] { 1f, .1f }).SetDuration(ttsDelayMillis)
+                .SetInterpolatorType(VolumeInterpolatorType.Linear).Build();
+
+            delayHandler = new Handler(Looper.MyLooper());
+
             paramsBundle = new Bundle();
             paramsBundle.PutString(TextToSpeech.Engine.KeyParamUtteranceId, "");
 
-            tts = new TextToSpeech(this, null);
+            tts = new TextToSpeech(this, new TTSInitListener(() =>
+            {
+                tts.SetSpeechRate(.8f);
+                tts.SetPitch(.9f);
+            }),
+            "com.google.android.tts");
+
             tts.SetOnUtteranceProgressListener(new TTSProgressListener(id => 
             {
-                bool subText = prevSubText != null && id == prevSubText;
-                bool text = prevText != null && prevSubText == null && id == prevText;
-                bool title = prevTitle != null && prevText == null && prevSubText == null && id == prevTitle;
+                bool text = prevText != null && id == prevText;
+                bool title = prevTitle != null && prevText == null && id == prevTitle;
 
-                if (title || text || subText) SongPlayer.mediaPlayer.SetVolume(1f, 1f);
+                if (title || text) delayHandler.PostDelayed(() => volumeShaper.Apply(VolumeShaper.Operation.Reverse), ttsDelayMillis);
             }));
         }
         public override void OnListenerDisconnected()
---
+++ AudioHub/TTSInitListener.cs
@@ -0,0 +1,28 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Speech.Tts;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AudioHub
+{
+    public class TTSInitListener : Java.Lang.Object, TextToSpeech.IOnInitListener
+    {
+        private Action onInit;
+
+        public TTSInitListener(Action onInit)
+        {
+            this.onInit = onInit;
+        }
+        public void OnInit(OperationResult status)
+        {
+            onInit();
+        }
+    }
+}
